2e1d3643fd8f2fcc8e77a7687555fd6830b2b6a5
qesito <qesito@qeso.com>
2025-01-20 17:49:53 -0600

    promedio java

---
+++ Promedio.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Promedio{ 
+	public static void main (String[] args) {
+
+	int a, b, c;
+        double	promedio;
+
+	Scanner teclado = new Scanner(System.in);
+	System.out.println("Ingresa el primer número: ");
+	a = teclado.nextInt();
+	
+	System.out.println("Ingresa el segundo número: ");
+	b = teclado.nextInt();
+	
+	System.out.println("Ingresa el tercer número: ");
+	c = teclado.nextInt();
+
+	promedio = (a+b+c) / 3.0;
+	 
+	System.out.println("el promedio es: " + promedio);
+
+	teclado.close();
+ }
+}
+# java-logica-de-programacion
